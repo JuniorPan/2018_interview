da9f8db8e2e8b87f13ce619e41b4954c32df874e
JuniorPan <panqiang0410@gmail.com>
2020-09-07 09:31:05 +0800

    add leetcode 23

--- leetcode/23.merge-k-sorted-lists.cpp
+++ leetcode/23.merge-k-sorted-lists.cpp
@@ -44,9 +44,50 @@
 class Solution
 {
 public:
-    // 解法二 
-    //利用了最小堆这种数据结构，首先把k个链表的首元素都加入最小堆中，它们会自动排好序。然后每次取出最小的那个元素加入最终结果的链表中，然后把取出元素的下一个元素再加入堆中，下次仍从堆中取出最小的元素做相同的操作，以此类推，直到堆中没有元素了，此时k个链表也合并为了一个链表，返回首节点即可
+    // 解法一
+    // 不停的对半划分，比如k个链表先划分为合并两个 k/2 个链表的任务，再不停的往下划分，直到划分成只有一个或两个链表的任务，开始合并。举个例子来说比如合并6个链表，那么按照分治法，首先分别合并0和3，1和4，2和5。这样下一次只需合并3个链表，再合并1和3，最后和2合并就可以了。代码中的k是通过 (n+1)/2 计算的，这里为啥要加1呢，这是为了当n为奇数的时候，k能始终从后半段开始，比如当 n=5 时，那么此时 k=3，则0和3合并，1和4合并，最中间的2空出来。当n是偶数的时候，加1也不会有影响，比如当 n=4 时，此时 k=2，那么0和2合并，1和3合并，完美解决问题
     ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        if (lists.empty())
+            return NULL;
+        int n = lists.size();
+        while (n > 1)
+        {
+            int k = (n + 1) / 2;
+            for (int i = 0; i < n / 2; ++i)
+            {
+                lists[i] = mergeTwoLists(lists[i], lists[i + k]);
+            }
+            n = k;
+        }
+        return lists[0];
+    }
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        ListNode *dummy = new ListNode(-1), *cur = dummy;
+        while (l1 && l2)
+        {
+            if (l1->val < l2->val)
+            {
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        if (l1)
+            cur->next = l1;
+        if (l2)
+            cur->next = l2;
+        return dummy->next;
+    }
+    // 解法二
+    //利用了最小堆这种数据结构，首先把k个链表的首元素都加入最小堆中，它们会自动排好序。然后每次取出最小的那个元素加入最终结果的链表中，然后把取出元素的下一个元素再加入堆中，下次仍从堆中取出最小的元素做相同的操作，以此类推，直到堆中没有元素了，此时k个链表也合并为了一个链表，返回首节点即可
+    ListNode *mergeKLists_2(vector<ListNode *> &lists)
     {
         auto cmp = [](ListNode *&a, ListNode *&b) {
             return a->val > b->val;
