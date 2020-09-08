0c3b7095e4341fa1f575076e073528b0d2581f5d
JuniorPan <panqiang0410@gmail.com>
2020-09-08 09:08:54 +0800

    add leetcode linklist 612

---
+++ leetcode/82.remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=82 lang=cpp
+ *
+ * [82] Remove Duplicates from Sorted List II
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
+ *
+ * algorithms
+ * Medium (36.26%)
+ * Likes:    1903
+ * Dislikes: 112
+ * Total Accepted:    263.3K
+ * Total Submissions: 709K
+ * Testcase Example:  '[1,2,3,3,4,4,5]'
+ *
+ * Given a sorted linked list, delete all nodes that have duplicate numbers,
+ * leaving only distinct numbers from the original list.
+ * 
+ * Return the linked list sorted as well.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2->3->3->4->4->5
+ * Output: 1->2->5
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->1->1->2->3
+ * Output: 2->3
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if (!head || !head->next)
+            return head;
+        ListNode *dummy = new ListNode(-1), *pre = dummy;
+        dummy->next = head;
+        while (pre->next)
+        {
+            ListNode *cur = pre->next;
+            while (cur->next && cur->next->val == cur->val)
+            {
+                cur = cur->next;
+            }
+            if (cur != pre->next)
+                pre->next = cur->next;
+            else
+                pre = pre->next;
+        }
+        return dummy->next;
+    }
+};
+// @lc code=end
