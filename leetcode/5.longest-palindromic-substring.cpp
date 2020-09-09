2e074d00e25f006dcaa13fb22f0c15e131f76202
JuniorPan <panqiang0410@gmail.com>
2020-09-09 23:10:26 +0800

    add leetcode dp 132

---
+++ leetcode/5.longest-palindromic-substring.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=5 lang=cpp
+ *
+ * [5] Longest Palindromic Substring
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * algorithms
+ * Medium (29.25%)
+ * Likes:    7850
+ * Dislikes: 567
+ * Total Accepted:    1M
+ * Total Submissions: 3.5M
+ * Testcase Example:  '"babad"'
+ *
+ * Given a string s, find the longest palindromic substring in s. You may
+ * assume that the maximum length of s is 1000.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "cbbd"
+ * Output: "bb"
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+    }
+};
+// @lc code=end
+
