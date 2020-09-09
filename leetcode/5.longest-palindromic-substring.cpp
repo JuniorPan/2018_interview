f5bf52ba1f360df33b3645c539864f23527f6263
JuniorPan <panqiang0410@gmail.com>
2020-09-09 23:32:15 +0800

    add leetcode dp 05

--- leetcode/5.longest-palindromic-substring.cpp
+++ leetcode/5.longest-palindromic-substring.cpp
@@ -34,11 +34,39 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    string longestPalindrome(string s) {
+    string longestPalindrome(string s)
+    {
         
+        // dp[i, j] = 1                                   if i == j
+        //          = s[i] == s[j]                        if j = i + 1
+        //          = s[i] == s[j] && dp[i + 1][j - 1]    if j > i + 1
+
+        if (s.empty())
+            return "";
+
+        int len = 0;
+        int left = 0;
+        int right = 0;
+        // dp[i][j] 表示字符串区间 [i, j] 是否为回文串
+        vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+        for (int j = 0; j < s.size(); j++) // 遍历所有的子串
+        {
+            for (int i = 0; i < j; i++)
+            {
+                dp[i][j] = s[i] == s[j] && (j - i < 2 || dp[i + 1][j - 1]);
+                if (dp[i][j] && len < j - i + 1)
+                {
+                    len = j - i + 1;
+                    left = i;
+                    right = j;
+                }
+            }
+            dp[j][j] = true;
+        }
+        return s.substr(left, right - left + 1);
     }
 };
 // @lc code=end
-
