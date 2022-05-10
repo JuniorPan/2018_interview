f16f5a00be05b6cca72caabc9e9226eef118dd97
JuniorPan <panqiang0410@gmail.com>
2022-05-10 23:42:25 +0800

    小镇刷题家他又来了

--- leetcode/56.merge-intervals.cpp
+++ leetcode/56.merge-intervals.cpp
@@ -5,9 +5,11 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
         if (intervals.size() == 0)
         {
             return {};
@@ -17,12 +19,12 @@ public:
         // 2: 如果当前区间的左端点在merged数组中最后一个区间的右端点之前, 需要更新当前区间的右端点更新数组中merged中最后一个区间的右端点，取二者的最大值
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> merged;
-        for(int i = 0; i < intervals.size(); i++)
+        for (int i = 0; i < intervals.size(); i++)
         {
             int left = intervals[i][0];
             int right = intervals[i][1];
 
-            if(!merged.size() || merged.back()[1] < left) // 条件1，不会重合
+            if (!merged.size() || merged.back()[1] < left) // 条件1，不会重合
                 merged.push_back({left, right});
             else // 条件2 重合,已有区间的右端点和当前右端点取最大值
                 merged.back()[1] = max(merged.back()[1], right);
@@ -31,4 +33,3 @@ public:
     }
 };
 // @lc code=end
-
