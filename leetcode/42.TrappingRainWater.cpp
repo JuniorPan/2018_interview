ba51b4b0ef7ed20b2cb6c2f2ed233e6a7df3aee6
JuniorPan <panqiang0410@gmail.com>
2023-01-07 10:25:50 +0800

    小镇刷题家他又来了

--- leetcode/42.TrappingRainWater.cpp
+++ leetcode/42.TrappingRainWater.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <stack>
 using namespace std;
 
 
 
-int trap(vector<int>& height) {
-    if (height.empty())
-        return 0;
-    
-    int n = height.size();
-    int res = 0;
-    int left_max = height[0];
-    int right_max = height[n-1];
-    
-    int l = 1;
-    int r = n - 2;
-    while(l <= r)
+int trap_1(vector<int> &height)
     {
-        if (left_max <= right_max)
+        if (height.empty())
+            return 0;
+        int res = 0;
+        int i = 0;
+
+        stack<int> monoStack;
+        // height.push_back(0);
+        for (int i = 0; i < height.size(); i++)
         {
-            res += max(0, left_max - height[l]);
-            left_max = max(left_max, height[l++]);
+            while (!monoStack.empty() && height[i] > height[monoStack.top()]) // 但栈非空时，且当前元素大于栈顶元素时，进行弹出操作，并且结算该弹出元素
+            // 栈顶元素的下一个元素则为左边界，当前遍历到的height[i]则为右边界
+            {
+                int tmp = monoStack.top();
+                monoStack.pop();
+                if (monoStack.empty())
+                    continue;
+
+                int h = min(height[i], height[monoStack.top()]);
+
+                cout << "i: " << i <<  " cur_area: " << (h - height[tmp]) * (i - monoStack.top() - 1) << endl;
+                res = res + (h - height[tmp]) * (i - monoStack.top() - 1);
+            }
+            monoStack.push(i);
         }
-        else
-        {
-            res += max(0, right_max - height[r]);
-            right_max = max(height[r--], right_max);
-        }
+        return res;
     }
-    return res;
+
+int main()
+{
+
+
+    vector<int> height = {2,1,0,1,3};
+    int res = trap_1(height);
+    cout << "res: " << res << endl;
+    return 0;
 }
