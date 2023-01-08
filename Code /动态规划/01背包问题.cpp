74cb862ea0b27aebfc3b04c418a7b5731fae8a9a
JuniorPan <panqiang0410@gmail.com>
2023-01-08 19:24:43 +0800

    小镇刷题家他又来了

--- Code /动态规划/01背包问题.cpp
+++ Code /动态规划/01背包问题.cpp
@@ -8,40 +8,70 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int number = 4;
-int capacity = 8;
 
-void findMax(int *w, int *v, int **V)
+// 0-1 背包二维数组解法
+void bag_problem_1()
 {
-    for(int i = 1; i <= number; i++)
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+
+    // dp[i][j] 表示 物品[0...i]中任取若干商品,放进容量为j的背包中，最大的价值
+    vector<vector<int>> dp(weight.size() + 1, vector<int>(bagWeight + 1, 0));
+    
+    //i为0，存放编号0的物品的时候，各个容量的背包所能存放的最⼤价值。
+    for(int j = 1; j <= bagWeight; j++)
     {
-        for (int j = 1; j <= capacity; j++)
+        
+        dp[0][j] = value[0];
+    }
+
+    for(int i = 1; i <= weight.size(); i++)
+    {
+        for(int j = 0; j <= bagWeight; j++)
         {
-            if (j < w[i])
-            {
-                V[i][j] = V[i-1][j];
-            }
+            if (j < weight[i])
+                dp[i][j] = dp[i-1][j];
             else
-            {
-                if (V[i-1][j] > V[i-1][j-w[i]] + v[i])
-                {
-                    V[i][j] = V[i-1][j];
-                }
-                else
-                {
-                    V[i][j] = V[i-1][j-w[i]] + v[i];
-                }
-            }
+                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i]);
+
         }
     }
+
+    cout << dp[weight.size()-1][bagWeight] << endl;
+
+}
+
+void bag_problem_2()
+{
+    vector<int> weight = {1, 3, 4};
+    vector<int> value = {15, 20, 30};
+    int bagWeight = 4;
+
+    // dp[j] 表示 容量为j的背包所背的最⼤价值
+    vector<int> dp(bagWeight + 1, 0);
     
+    
+    for(int i = 0; i < weight.size(); i++)
+    {
+        for(int j = bagWeight; j >= 0; j--)
+        {   
+            if (j - weight[i] >= 0)
+                dp[j] = max(dp[j], dp[j-weight[i]] + value[i]);
+        }
+    }
+
+    cout << dp[bagWeight] << endl;
 }
 
 
-
-int main1()
-{
+int main()
+{   
+    bag_problem_1();
+    bag_problem_2();
     
     cout << "hello world" << endl;
     
