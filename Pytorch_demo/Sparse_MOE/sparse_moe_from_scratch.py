f569ee58f3c7bc9195149e16f23987b93223163e
JuniorPan <panqiang0410@gmail.com>
2024-04-07 22:29:26 +0800

    小镇刷题家他又来了

---
+++ Pytorch_demo/Sparse_MOE/sparse_moe_from_scratch.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+torch.manual_seed(42)
+#Optional
+import mlflow
+
+
+n_embd = 64
+n_head = 4
+n_layer = 4
+head_size = 16
+dropout = 0.1
+block_size = 8
+
+class Head(nn.Module):
+    """ one head of self-attention """
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embd, head_size, bias=False)
+        self.query = nn.Linear(n_embd, head_size, bias=False)
+        self.value = nn.Linear(n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        B,T,C = x.shape
+        k = self.key(x)   # (B,T,C)
+        q = self.query(x) # (B,T,C)
+        # compute attention scores ("affinities")
+        wei = q @ k.transpose(-2,-1) * C**-0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
+        # perform the weighted aggregation of the values
+        v = self.value(x) # (B,T,C)
+        out = wei @ v # (B, T, T) @ (B, T, C) -> (B, T, C)
+        return out
+    
+#Multi-Headed Self Attention
+class MultiHeadAttention(nn.Module):
+    """ multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+#Expert module
+class Expert(nn.Module):
+    """ An MLP is a simple linear layer followed by a non-linearity i.e. each Expert """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+# First define the top k router module
+class TopkRouter(nn.Module):
+    def __init__(self, n_embed, num_experts, top_k):
+        super(TopkRouter, self).__init__()
+        self.top_k = top_k
+        self.linear =nn.Linear(n_embed, num_experts)
+
+    def forward(self, mh_ouput):
+        # mh_ouput is the output tensor from multihead self attention block
+        logits = self.linear(mh_output)
+        top_k_logits, indices = logits.topk(self.top_k, dim=-1)
+        zeros = torch.full_like(logits, float('-inf'))
+        sparse_logits = zeros.scatter(-1, indices, top_k_logits)
+        router_output = F.softmax(sparse_logits, dim=-1)
+        return router_output, indices
+    
+
+#Changing the above to accomodate noisy top-k gating
+class NoisyTopkRouter(nn.Module):
+    def __init__(self, n_embed, num_experts, top_k):
+        super(NoisyTopkRouter, self).__init__()
+        self.top_k = top_k
+        #layer for router logits
+        self.topkroute_linear = nn.Linear(n_embed, num_experts)
+        self.noise_linear =nn.Linear(n_embed, num_experts)
+
+
+    def forward(self, mh_output):
+        # mh_ouput is the output tensor from multihead self attention block
+        logits = self.topkroute_linear(mh_output)
+
+        #Noise logits
+        noise_logits = self.noise_linear(mh_output)
+
+        #Adding scaled unit gaussian noise to the logits
+        noise = torch.randn_like(logits)*F.softplus(noise_logits)
+        noisy_logits = logits + noise
+
+        top_k_logits, indices = noisy_logits.topk(self.top_k, dim=-1)
+        zeros = torch.full_like(noisy_logits, float('-inf'))
+        sparse_logits = zeros.scatter(-1, indices, top_k_logits)
+        router_output = F.softmax(sparse_logits, dim=-1)
+        return router_output, indices
+    
+class SparseMoE(nn.Module):
+    def __init__(self, n_embed, num_experts, top_k):
+        super(SparseMoE, self).__init__()
+        self.router = NoisyTopkRouter(n_embed, num_experts, top_k)
+        self.experts = nn.ModuleList([Expert(n_embed) for _ in range(num_experts)])
+        self.top_k = top_k
+
+    def forward(self, x):
+        print('x.shape: ', x.shape)
+        gating_output, indices = self.router(x)
+        
+        print('gating_output: ', gating_output)
+        print('indices: ', indices)
+        final_output = torch.zeros_like(x)
+        print('final_output.shape: ', final_output.shape)
+
+        # Reshape inputs for batch processing
+        flat_x = x.view(-1, x.size(-1))
+        
+        
+        flat_gating_output = gating_output.view(-1, gating_output.size(-1))
+        
+        print('flat_x.shape: ', flat_x.shape)
+        print('flat_gating_output.shape: ', flat_gating_output.shape)
+
+        # Process each expert in parallel
+        for i, expert in enumerate(self.experts):
+            # Create a mask for the inputs where the current expert is in top-k
+            expert_mask = (indices == i).any(dim=-1)
+            print('expert_mask: ', expert_mask, ' export i: ', i)
+            flat_mask = expert_mask.view(-1)
+            # print('flat_mask: ', flat_mask)
+
+            if flat_mask.any():
+                expert_input = flat_x[flat_mask]
+                print('expert_input: ', expert_input)
+                expert_output = expert(expert_input)
+                # Extract and apply gating scores
+                gating_scores = flat_gating_output[flat_mask, i].unsqueeze(1)
+                weighted_output = expert_output * gating_scores
+                # Update final output additively by indexing and adding
+                final_output[expert_mask] += weighted_output.squeeze(1)
+
+        return final_output
+
+
+#Testing this out, again:
+#Let's test this out
+num_experts = 8
+top_k = 3
+n_embd = 4
+dropout=0.1
+
+mh_output = torch.randn(1, 8, n_embd)  # Example multi-head attention output
+sparse_moe = SparseMoE(n_embd, num_experts, top_k)
+final_output = sparse_moe(mh_output)
+print("Shape of the final output:", final_output.shape)
+print(final_output)
+
+
